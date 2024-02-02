78338116360875f57838668ff5fcfca97c8c8023
Arnab Jana <arnabjana0099@gmail.com>
2024-02-03 00:03:37 +0530

    ck

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   console.log(process.env.MONGODB_URL);
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(String(process.env.MONGODB_URL));
     console.log("MongoDB connected");
   } catch (err) {
     console.log("DB connection error::", err);
